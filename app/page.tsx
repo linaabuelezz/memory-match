109f06d4cddfa15a0daeef6eeeebad320333324e
Lina Abuelezz <labuelez@uoguelph.ca>
2024-08-05 14:15:02 +0300

    fixing css spacing for game page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MemoryGame from "@/components/MemoryGame";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <MemoryGame />
     </main>
   );
